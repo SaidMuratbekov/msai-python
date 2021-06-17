cb7745ff072a4e4b94b53053ebbbd7957e52b3f2
SaidMuratbekov <saidmuratbekov@gmail.com>
2021-06-17 19:39:56 +0500

    HW1.1

---
+++ HW/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import methods_importer # noqa
+from . import * # noga
--- week1_00_packages/project/project_file_1.py
+++ week1_00_packages/project/project_file_1.py
@@ -1,5 +1,7 @@
 # project_file_1.py
 import itertools
+import sys
+print(sys.path)
 
 def project_file_func():
     print("Simple project file func")
